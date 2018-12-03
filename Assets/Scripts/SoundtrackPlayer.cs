c56f4035a51d5bd09e6a06a41f7737c5b2e4d778
ibimsderalex <alexander.buechler25@gmail.com>
2018-12-03 13:15:36 +0100

    added soundtrack and distance limit to core

---
+++ Assets/Scripts/SoundtrackPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackPlayer : MonoBehaviour
+{
+    public AudioClip[] musicTracks = new AudioClip[2];
+
+    public AudioSource musicPlayer;
+
+    private int index;
+
+    private void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+    }
+
+    private void LateUpdate()
+    {
+        if (!musicPlayer.isPlaying)
+        {
+            index++;
+            playMusic(index);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void playMusic(int index)
+    {
+        musicPlayer.PlayOneShot(musicTracks[index]);
+    }
+
+}
--- Assets/Scripts/rangeDetector.cs
+++ Assets/Scripts/rangeDetector.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class rangeDetector : MonoBehaviour
 {
 
+    public float maxDistance;
+    public float currentDistance;
+
     public CoreBehaviour core;
     public WarningBehaviour warning;
+    public Transform shipTransform;
+    public AudioSource shipAudio;
+
+    [SerializeField]
+    private AudioClip warningSound;
+
 
 	void Start ()
     {
         core = GetComponentInParent<CoreBehaviour>();
         warning = GameObject.Find("WarningSign").GetComponent<WarningBehaviour>();
+        shipTransform = GameObject.Find("Ship").transform;
+        shipAudio = GameObject.Find("Ship").GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        currentDistance = Vector3.Distance(transform.position, shipTransform.position);
+
+        if (currentDistance > maxDistance)
+        {
+            Debug.Log("too far away bro!");
+        }
 	}
 
     private void OnTriggerStay(Collider other)
@@ -24,6 +41,21 @@ public class rangeDetector : MonoBehaviour
         if (other.tag == "Enemy")
         {
             warning.Warning();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (!shipAudio.isPlaying)
+            {
+                shipAudio.PlayOneShot(warningSound, 1f);
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
